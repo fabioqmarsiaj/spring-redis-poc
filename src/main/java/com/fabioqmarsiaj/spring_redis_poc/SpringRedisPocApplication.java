4b640bfda2c049a0e9803e20d3a5363adbad8967
Fabio Marsiaj <fabiobackend@gmail.com>
2025-03-05 17:12:19 -0300

    first commit

---
+++ src/main/java/com/fabioqmarsiaj/spring_redis_poc/SpringRedisPocApplication.java
@@ -0,0 +1,13 @@
+package com.fabioqmarsiaj.spring_redis_poc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRedisPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRedisPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fabioqmarsiaj/spring_redis_poc/SpringRedisPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fabioqmarsiaj.spring_redis_poc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRedisPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
